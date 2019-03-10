1c9400538c36608ddddaefcbb2bc68228cc0c0f7
Sesamestrong <doolie.healey@gmail.com>
2019-03-10 19:29:26 -0400

    Added another proxy.

--- proxies.js
+++ proxies.js
@@ -1,2 +1,2 @@
 let makeCorsProxy=proxy=>url=>"https://"+proxy+".herokuapp.com/"+url;
-let proxyFinder = [(url => url), makeCorsProxy("cors-anywhere"), makeCorsProxy("corsanywhere"),makeCorsProxy("cors-any-where"),makeCorsProxy("corsany-where"),makeCorsProxy("anywhere-cors"),makeCorsProxy("c-o-r-s-anywhere")];
+let proxyFinder = [(url => url), makeCorsProxy("cors-anywhere"), makeCorsProxy("corsanywhere"),makeCorsProxy("cors-any-where"),makeCorsProxy("corsany-where"),makeCorsProxy("anywhere-cors"),makeCorsProxy("c-o-r-s-anywhere"),makeCorsProxy("any-where-cors")];
